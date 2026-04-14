e8101e83c8d476ae6c71cd687cf4bca63976a787
Bhupendra Singh Koranga <bhupendra.singh@brainnetic.com>
2026-04-14 13:00:59 +0530

    swagger added

--- BlogService.API/Program.cs
+++ BlogService.API/Program.cs
@@ -121,11 +121,10 @@ var app = builder.Build();
 app.UseExceptionHandler();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 
